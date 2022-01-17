17f909368ea77baa0b465b8273cab4d34a138574
Fen1xx <provkovasu15@gmail.com>
2022-01-17 20:59:10 +0500

    Webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,49 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
     devtool: 'source-map',
     devServer: {
         hot:false,
         liveReload: true
     },
-    entry: './src/js/index.js',
+    entry: './js/index.js',
     output: {
         filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
         clean: true
     },
     module: {
         rules: [
             {
-                test: /\.css/,
+                test: /\.html$/i,
+                use: 'html-loader'
+            },
+            {
+                test: /\.css$/i,
                 use: [
-                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    // 'style-loader',
                     'css-loader'
-                ]
-            }
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './index.html'
         }),
+        new MiniCssExtractPlugin(),
     ],
 };
